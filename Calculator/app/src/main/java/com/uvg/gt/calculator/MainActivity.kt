fdf9ef0279d85b513f0746b0e2caadc970d9db57
nicollegordillo <98134296+nicollegordillo@users.noreply.github.com>
2023-08-05 10:34:20 -0600

    UI calculadora

--- Calculator/app/src/main/java/com/uvg/gt/calculator/MainActivity.kt
+++ Calculator/app/src/main/java/com/uvg/gt/calculator/MainActivity.kt
@@ -2,10 +2,161 @@ package com.uvg.gt.calculator
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+enum class ExpressionElement {
+    NUMERO,
+    MAS,
+    MENOS,
+    MULTI,
+    DIV,
+    MOD,
+    POTENCIA,
+    OPEN_PARENTHESIS,
+    CLOSE_PARENTHESIS
+}
 class MainActivity : AppCompatActivity() {
+    private lateinit var textField: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val equalsButton: Button = findViewById(R.id.button17)
+        // onClick del boton =
+        equalsButton.setOnClickListener {
+            onEqualsButtonClick(it)
+        }
+
+        val clear: Button = findViewById(R.id.button20)
+
+        // onClick del boton clear
+        clear.setOnClickListener {
+            onClearButtonClick(it)
+        }
+
+        val borrar: Button = findViewById(R.id.button19)
+
+        // onClick del boton borrar
+        borrar.setOnClickListener {
+            onBackspaceButtonClick(it)
+        }
+        textField = findViewById(R.id.textField)
+
+        // Set clicklisteners de todos los botones
+        setButtonClickListeners()
+    }
+
+    private fun setButtonClickListeners() {
+        val buttonClickListener = View.OnClickListener { v ->
+            val button = v as Button
+            val buttonText = button.text.toString()
+
+            // se pone el texto del boton en el textfield
+            textField.append(buttonText)
+        }
+
+        // listeners de los números
+        findViewById<Button>(R.id.button).setOnClickListener(buttonClickListener)
+        findViewById<Button>(R.id.button2).setOnClickListener(buttonClickListener)
+        findViewById<Button>(R.id.button3).setOnClickListener(buttonClickListener)
+        findViewById<Button>(R.id.button4).setOnClickListener(buttonClickListener)
+        findViewById<Button>(R.id.button5).setOnClickListener(buttonClickListener)
+        findViewById<Button>(R.id.button6).setOnClickListener(buttonClickListener)
+        findViewById<Button>(R.id.button7).setOnClickListener(buttonClickListener)
+        findViewById<Button>(R.id.button8).setOnClickListener(buttonClickListener)
+        findViewById<Button>(R.id.button9).setOnClickListener(buttonClickListener)
+        findViewById<Button>(R.id.button10).setOnClickListener(buttonClickListener)
+
+        // listeners de los símbolos
+        findViewById<Button>(R.id.button13).setOnClickListener(buttonClickListener) // *
+        findViewById<Button>(R.id.button14).setOnClickListener(buttonClickListener) // /
+        findViewById<Button>(R.id.button15).setOnClickListener(buttonClickListener) // %
+        findViewById<Button>(R.id.button16).setOnClickListener(buttonClickListener) // ^
+        findViewById<Button>(R.id.button11).setOnClickListener(buttonClickListener) // +
+        findViewById<Button>(R.id.button12).setOnClickListener(buttonClickListener) // -
+        findViewById<Button>(R.id.button18).setOnClickListener(buttonClickListener) // .
+        findViewById<Button>(R.id.button21).setOnClickListener(buttonClickListener) // (
+        findViewById<Button>(R.id.button22).setOnClickListener(buttonClickListener) // )
+    }
+
+    // =
+    fun onEqualsButtonClick(view: View) {
+        val expressionText = textField.text.toString()
+
+        // Array que guarda los enum de los elementos
+        val expressionArray = mutableListOf<ExpressionElement>()
+
+        var currentNumber = StringBuilder()
+
+        fun addNumberToExpression() {
+            if (currentNumber.isNotEmpty()) {
+                expressionArray.add(ExpressionElement.NUMERO)
+                currentNumber.clear()
+            }
+        }
+
+        for (char in expressionText) {
+            when (char) {
+                '+' -> {
+                    addNumberToExpression()
+                    expressionArray.add(ExpressionElement.MAS)
+                }
+                '-' -> {
+                    addNumberToExpression()
+                    expressionArray.add(ExpressionElement.MENOS)
+                }
+                '*' -> {
+                    addNumberToExpression()
+                    expressionArray.add(ExpressionElement.MULTI)
+                }
+                '/' -> {
+                    addNumberToExpression()
+                    expressionArray.add(ExpressionElement.DIV)
+                }
+                '%' -> {
+                    addNumberToExpression()
+                    expressionArray.add(ExpressionElement.MOD)
+                }
+                '^' -> {
+                    addNumberToExpression()
+                    expressionArray.add(ExpressionElement.POTENCIA)
+                }
+                '(' -> {
+                    addNumberToExpression()
+                    expressionArray.add(ExpressionElement.OPEN_PARENTHESIS)
+                }
+                ')' -> {
+                    addNumberToExpression()
+                    expressionArray.add(ExpressionElement.CLOSE_PARENTHESIS)
+                }
+                in '0'..'9', '.' -> {
+                    currentNumber.append(char)
+                }
+                else -> {
+                    // Ignora cualquier otra cosa
+                }
+            }
+        }
+
+        addNumberToExpression()
+
+        //for (element in expressionArray) {
+        //   println(element.toString())
+        //}
+    }
+
+    // CLEAR
+    fun onClearButtonClick(view: View) {
+        textField.text.clear()
+    }
+
+    // BORRAR
+    fun onBackspaceButtonClick(view: View) {
+        val currentText = textField.text.toString()
+        if (currentText.isNotEmpty()) {
+            textField.text.delete(currentText.length - 1, currentText.length)
+        }
     }
 }
